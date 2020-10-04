e74dc5377124e32407a6e3c3a16c5da3018dec5d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 05:12:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,16 +19,7 @@ class ApplicationController < Sinatra::Base
       erb :error
     end
   end
-
-# post '/login' do
-#     @user = User.find_by(username: params[:username])
-#     if @user
-#       session[:user_id] = @user.id
-#       redirect '/account'
-#     else
-#       erb :error
-#     end
-#   end
+  
   get '/account' do
      @current_user = User.find_by_id(session[:user_id])
     if @current_user
